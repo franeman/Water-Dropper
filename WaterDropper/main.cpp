8e6a89d359776371b8b8b3d805fa3299e274cd65
Franeman <franeman@yahoo.com>
2018-11-29 19:53:59 -0500

    Initial commit

---
+++ WaterDropper/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * WaterDropper.cpp
+ *
+ * Created: 11/13/2018 10:40:40 PM
+ * Author : Ethan
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
